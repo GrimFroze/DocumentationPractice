3a7684170ca600194ebc66ba0668bfcfb5871da4
GrimFroze <109207105+GrimFroze@users.noreply.github.com>
2025-10-20 09:31:00 +0700

    AKUUUUHHH CINTAAAHHH KAMUHHHHH

--- Main.java
+++ Main.java
@@ -1,15 +1,20 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello My Name Is Joko");
+        Scanner input = new Scanner(System.in);
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        // Ask for the user's name
+        System.out.print("Enter your name: ");
+        String name = input.nextLine();
+
+        // Ask for the user's age
+        System.out.print("Enter your age: ");
+        int age = input.nextInt();
+
+        // Print a message
+        System.out.println("Hello, " + name + "! You are " + age + " years old.");
+
+        input.close();
     }
 }
